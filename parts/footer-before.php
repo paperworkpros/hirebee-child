fd0aa57395537092d21859430feb6974877b6b84
seothemes <seothemeswp@gmail.com>
2021-04-19 21:38:46 +0800

    Before footer changes

--- parts/footer-before.php
+++ parts/footer-before.php
@@ -1,7 +1,7 @@
 <div class="before-footer">
 	<div class="row">
 		<div class="large-9 columns">
-			<h2 class="before-footer-title">The best way to find perfect no-code developers!</h2>
+			<h2 class="before-footer-title">The best way to find the perfect no-code developer!</h2>
 		</div>
 		<div class="large-3 columns">
 			<a href="/post-a-project" class="button alignright">Post a Project</a>
