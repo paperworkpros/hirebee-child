1bb3bd8ebc28258d45cd09bb58d7137dbd7fd9f2
seothemes <seothemeswp@gmail.com>
2021-04-19 21:45:41 +0800

    How it works section content

--- parts/how-it-works.php
+++ parts/how-it-works.php
@@ -8,22 +8,22 @@
 
 		<div class="large-3 columns icon-box">
 			<img src="<?php echo $stylesheet_uri; ?>/img/1.png" class="icon" alt="">
-			<p>Post projects to tell us what you need done</p>
+			<p>Post a project and receive up to five bids from qualified developers based in the U.S. and Canada.</p>
 		</div>
 
 		<div class="large-3 columns icon-box">
 			<img src="<?php echo $stylesheet_uri; ?>/img/2.png" class="icon" alt="">
-			<p>Browse profiles, reviews, then hire your most favorite and start project</p>
+			<p>Ask and answer questions from interested developers and “Accept” the bid you like the best.</p>
 		</div>
 
 		<div class="large-3 columns icon-box">
 			<img src="<?php echo $stylesheet_uri; ?>/img/3.png" class="icon" alt="">
-			<p>Use FreelanceEngine platform to chat and share files</p>
+			<p>Fund the project in small increments as you see progress through the secure Stripe Connect escrow.</p>
 		</div>
 
 		<div class="large-3 columns icon-box">
 			<img src="<?php echo $stylesheet_uri; ?>/img/4.png" class="icon" alt="">
-			<p>With our protection, money is only paid for work you authorize</p>
+			<p>Provide a review of the developer at the end of the project or “Dispute” the work at any time if you are unhappy.</p>
 		</div>
 
 	</div>
