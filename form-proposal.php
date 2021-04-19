4b98c5288710e5913883a522cffd6384f726c4cd
seothemes <seothemeswp@gmail.com>
2021-04-19 14:11:40 +0800

    Only show bid form if connected to stripe

--- form-proposal.php
+++ form-proposal.php
@@ -1,7 +1,20 @@
 <?php
 
-global $wp;
+if ( get_user_option( 'stripe-test-connected-account-id' ) ) {
+	global $wp;
 
-$project_id = (int) basename( $wp->request );
+	$project_id = (int) basename( $wp->request );
 
-gravity_form( 7, false, false );
+	gravity_form( 7, false, false );
+
+} else {
+	?>
+	<div data-alert="" class="notice error alert-box radius">
+		<div>Notice: You need to <a href="<?php echo home_url( 'dashboard/payments' ); ?>">connect your Stripe
+				account</a> before you can apply to projects.
+		</div>
+		<a href="#" class="close">×</a>
+	</div>
+	<?php
+
+}
